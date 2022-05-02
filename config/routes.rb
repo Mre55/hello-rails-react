67960d977412a18d405524b9c40c7a09f5bbc600
mre55 <misganawmihreteab>
2022-05-03 02:54:23 +0300

    fix linter error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: { format: :json } do
+    resources :greetings, only: [:index]
+  end
+  get 'static/index'
+
   root 'static#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  namespace :api, defaults: { format: :json } do
-    resources :greetings, only: [:index]
-  end
+ 
 end
