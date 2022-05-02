4aa1fbae551ba53cd0f8d75995cad7f74a7f97d2
mre55 <misganawmihreteab>
2022-05-03 01:33:44 +0300

    Create an API endpoint that selects a random greeting for that created greeting controller and update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'static#index'
+  
   root 'static#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :greetings, only: [:index]
+  end
 end
